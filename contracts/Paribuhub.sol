7dcb92dac26a15c847824437d460a553f793d9e2
Orhan Cansu <65315135+konerjonlar@users.noreply.github.com>
2023-10-09 17:16:19 +0300

    recode dinamic array

--- contracts/Paribuhub.sol
+++ contracts/Paribuhub.sol
@@ -106,8 +106,13 @@ contract ParibuHub is IAction, Whois, VeliUysal {
     }
 
 function getAllAdmins() public view returns (Account[] memory) {
-    return admins;
+    Account[] memory _admins = new Account[](admins.length);
+    for (uint i = 0; i < admins.length; i++) {
+        _admins[i] = admins[i];
+    }
+    return _admins;
 }
+
 
 
  function iAmReady() external pure returns(string memory){
